09c0d3d72ae744bf94318c1437920116c8c1b081
Polina Krasnoiartseva <polipolipoli@MacBook-Air-Polina.local>
2025-05-26 16:27:27 +0200

    images, pop up text and button

--- src/RunningLine.js
+++ src/RunningLine.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 export default function RunningLine() {
-  const text = "crée une carte → assemble ou laisse le hasard faire ⚄ → reçois-la par e-mail  |  craft a postcard → match two columns or go random ⚄ → get your card emailed to you  |  как сделать открытку? → создай пару сам или доверься случайному выбору ⚄ → получи письмо с открыткой  |  ";
+  const text = "crée une carte → assemble ou laisse le hasard faire ⚄ → reçois-la par e-mail  |  craft a postcard → match two columns or go random ⚄ → get your card emailed to you  |  как сделать открытку? → создай пару сам или доверься случайному выбору ⚄ → получи письмо с открыткой (нажимая на кнопку «send» ты соглашаешься с нашей политикой конфиденциальности)  |  ";
   const textRef = useRef(null);
   const [textWidth, setTextWidth] = useState(0);
 
